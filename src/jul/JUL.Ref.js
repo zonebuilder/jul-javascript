57ca4416af79ca0bd6be4511f67036bc3b8690ab
The Zonebuilder <zone.builder@gmx.com>
2016-10-03 11:08:45 +0300

    Release 1.3.6

--- src/jul/JUL.Ref.js
+++ src/jul/JUL.Ref.js
@@ -1,5 +1,5 @@
 /*
-	JUL - The JavaScript UI Language module version 1.3.5
+	JUL - The JavaScript UI Language module version 1.3.6
 	Copyright (c) 2012 - 2016 The Zonebuilder (zone.builder@gmx.com)
 	http://sourceforge.net/projects/jul-javascript/
 	Licenses: GNU GPL2 or later; GNU LGPLv3 or later (http://sourceforge.net/p/jul-javascript/wiki/License/)
